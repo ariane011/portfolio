bd6d096794b4c5ffcfad15234ec3a7797e698538
Ariane Gomes de Sousa <ariane.sousa011@gmail.com>
2020-05-21 13:15:29 -0300

    implementacao

---
+++ script.js
@@ -0,0 +1,13 @@
+function typeWrite(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function (letra, i) {
+
+        setTimeout(function () {
+            elemento.innerHTML += letra;
+        }, 85 * i)
+
+    });
+}
+const titulo = document.querySelector('.site-title', '.site-subtitle');
+typeWrite(titulo);
